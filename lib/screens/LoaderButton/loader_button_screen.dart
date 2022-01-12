bc935047769df098f69f2a975355fccf066f2abc
nyamadip <nyamadip@stanbic.com.gh>
2022-01-12 09:36:36 +0000

    loader button setup

---
+++ lib/screens/LoaderButton/loader_button_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoaderButtonScreen extends StatefulWidget {
+  const LoaderButtonScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoaderButtonScreenState createState() => _LoaderButtonScreenState();
+}
+
+class _LoaderButtonScreenState extends State<LoaderButtonScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
